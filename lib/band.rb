4a9fed86d5d59fee6ac8d2189b58757a88acffb0
Guest User <student@epicodus.com>
2015-05-15 09:44:11 -0700

    validate name and capitalize for band model

--- lib/band.rb
+++ lib/band.rb
@@ -1,4 +1,12 @@
 class Band < ActiveRecord::Base
   has_and_belongs_to_many :venues
   has_many :genres
+  validates :name, presence: true
+  before_save :capitalize
+
+private
+  def capitalize
+    split_name_capitalized = name.split.map { |word| word.capitalize! }
+    self.name = split_name_capitalized.join(' ')
+  end
 end
--- spec/band_spec.rb
+++ spec/band_spec.rb
@@ -2,5 +2,17 @@ require "spec_helper"
 
 describe(Band) do
   it { should have_and_belong_to_many(:venues) }
+
   it { should have_many(:genres) }
+
+  it("has a name") do
+    band = Band.new(name: "")
+    expect(band.save).to eq(false)
+  end
+
+  it("always has a capitalized name") do
+    band = Band.create(name: "fleet foxes")
+    expect(band.name).to eq("Fleet Foxes")
+  end
+
 end
